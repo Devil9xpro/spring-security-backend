15d32ac2f297477d9ed7193b2517b207ba9ecf9b
David Nguye <chinhzz3107@gmail.com>
2022-03-02 21:17:09 +0700

    add users in memory db

--- src/main/java/com/example/springsecuritybackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybackend/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.springsecuritybackend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,17 +27,17 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 //        custom configuration for requirement above
-//        http.authorizeRequests()
-//                .antMatchers("/myAccount").authenticated()
-//                .antMatchers("/myBalance").authenticated()
-//                .antMatchers("/myLoans").authenticated()
-//                .antMatchers("/myCards").authenticated()
-//                .antMatchers("/notices").permitAll()
-//                .antMatchers("/contact").permitAll()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
 
 //        configuration to deny all the requests
 //        http.authorizeRequests().anyRequest().denyAll()
@@ -45,11 +47,21 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .httpBasic();
 
 //        configuration to permit all the requests
-        http.authorizeRequests().anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
+//        http.authorizeRequests().anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("12345").authorities("admin")
+                .and()
+                .withUser("user").password("12345").authorities("read")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
